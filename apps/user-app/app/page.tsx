981528d430f0fa9eebf8169eef0f62efd4a41684
Inesh-Reddy <ineshreddy007@gmail.com>
2024-03-29 13:53:20 +0530

    adding a card component

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,8 +1,13 @@
+// import { PrismaClient } from "../../../packages/db";
+// import { PrismaClient } from "@repo/db/client";
+// const client = new PrismaClient();
 
 export default function Page(): JSX.Element {
   return (
-    <div className="text-2xl">
-      Paytm App!!!
+    <div className="container">
+      <div className="card">
+        <p className="text">Paytm App!!!</p>
+      </div>
     </div>
   );
 }
